06c0af7cccde4611bc394c13c2ad4d044565acb4
Isabel <105936212+iomullony@users.noreply.github.com>
2024-01-27 00:36:15 +0100

    Starting the backend

---
+++ backend/src/main/java/com/desk_sharing/App.java
@@ -0,0 +1,13 @@
+package com.desk_sharing;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
