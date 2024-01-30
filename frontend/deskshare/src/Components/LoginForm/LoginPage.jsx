b707e87a5e3f0db48cc9818f0161679c87b879bf
George Nyagura <97040916+georgeNyagura@users.noreply.github.com>
2024-01-30 15:43:08 +0200

    Validating email address enterd by user

--- frontend/deskshare/src/Components/LoginForm/LoginPage.jsx
+++ frontend/deskshare/src/Components/LoginForm/LoginPage.jsx
@@ -1,30 +1,65 @@
-import React from 'react';
-import './LoginPage.css';
+import React, { useState } from "react";
+import "./LoginPage.css";
 import { FaUser } from "react-icons/fa";
 import { FaLock } from "react-icons/fa";
-import flagImage from '../../images/flag.png'; // Adjusted import path
+import flagImage from "../../images/flag.png"; // Adjusted import path
 
 const LoginPage = () => {
+  const [values, setValues] = useState({ email: "", password: "" });
+  const [errors, setErrors] = useState({});
+  const isEmail = (email) =>
+    /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email);
+
+  function submitForm(e) {
+    const invalidFields = {};
+    if (!isEmail(values.email)) {
+      invalidFields.email = "Wrong email";
+    }
+    setErrors(invalidFields);
+    if (!Object.keys(invalidFields).length) {
+      alert(JSON.stringify(values));
+    } else {
+      alert(JSON.stringify(invalidFields.email));
+      console.log("invalid");
+      // console.log("email: " + values.email);
+      // console.log("password: " + values.password);
+    }
+
+    e.preventDefault();
+  }
   return (
-    <div className='wrapper'>
-      <img src={flagImage} alt="Flag" className="flag-image" /> {/* Using the flag.png image */}
+    <div className="wrapper">
+      <img src={flagImage} alt="Flag" className="flag-image" />{" "}
+      {/* Using the flag.png image */}
       <form action="">
         <h1>Login</h1>
-        <div className='input-box'>
-          <input type="text" placeholder='Username' required/>
-          <FaUser className='icon'/>
+        <div className="input-box">
+          <input
+            onChange={(e) => setValues({ ...values, email: e.target.value })}
+            type="text"
+            placeholder="Username"
+            required
+          />
+          <FaUser className="icon" />
         </div>
-        <div className='input-box'>
-          <input type="password" placeholder='Password' required/> 
-          <FaLock className='icon'/>
+        <div className="input-box">
+          <input
+            onChange={(e) => setValues({ ...values, password: e.target.value })}
+            type="password"
+            placeholder="Password"
+            required
+          />
+          <FaLock className="icon" />
         </div>
-        <div className='forgot-password'> 
-          <a href="#">Forgot Password ?</a>
+        <div className="forgot-password">
+          <a href="/">Forgot Password ?</a>
         </div>
-        <button type='submit'>Login</button>
+        <button type="submit" onClick={(e) => submitForm(e)}>
+          Login
+        </button>
       </form>
     </div>
   );
-}
+};
 
 export default LoginPage;
