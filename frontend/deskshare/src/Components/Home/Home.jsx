93140d32929c7e28454d7e7ac6555cfacf9e4b87
George Nyagura <97040916+georgeNyagura@users.noreply.github.com>
2024-01-31 22:31:37 +0200

    Added Sidebar component to the home page

--- frontend/deskshare/src/Components/Home/Home.jsx
+++ frontend/deskshare/src/Components/Home/Home.jsx
@@ -1,10 +1,24 @@
 import React from "react";
 import "./Home.css";
 
+import { Sidebar, Menu, MenuItem, SubMenu } from "react-pro-sidebar";
+
 export default function Home() {
   return (
     <div className="home-page">
-      <div className="sidebar"></div>
+      <div className="sidebar">
+        <Sidebar>
+          <Menu>
+            <SubMenu label="Display Bookings">
+              <MenuItem> 12/12/24 </MenuItem>
+              <MenuItem> 12/12/24 </MenuItem>
+            </SubMenu>
+            <MenuItem> Profile </MenuItem>
+            <MenuItem> Calendar </MenuItem>
+          </Menu>
+        </Sidebar>
+        ;
+      </div>
       <div className="home-content"></div>
     </div>
   );
