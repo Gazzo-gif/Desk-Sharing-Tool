62790995d0b6b214b22d9da81b6d6cded72eadc6
George Nyagura <97040916+georgeNyagura@users.noreply.github.com>
2024-02-01 05:14:32 +0200

    Adding events is now possible

--- frontend/deskshare/src/Components/Home/HomeCalendar.jsx
+++ frontend/deskshare/src/Components/Home/HomeCalendar.jsx
@@ -1,6 +1,8 @@
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
 import "./HomeCalendar.scss";
+import CalendarEvents from "./CalendarEvents";
+
 const localizer = momentLocalizer(moment);
 
 const HomeCalendar = (props) => (
@@ -12,7 +14,7 @@ const HomeCalendar = (props) => (
     <div>
       <Calendar
         localizer={localizer}
-        events={""}
+        events={CalendarEvents}
         startAccessor="start"
         endAccessor="end"
         style={{ height: 500 }}
