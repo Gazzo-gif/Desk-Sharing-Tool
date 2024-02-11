f6a45a5175b95496ec4e9d06ea7fa4e8c4bf4d82
gazzo-desktop <159712495+gazzo-desktop@users.noreply.github.com>
2024-02-11 21:12:18 +0500

    Room and Desk APIs

---
+++ backend/src/main/java/com/desk_sharing/controllers/DeskController.java
@@ -0,0 +1,51 @@
+package com.desk_sharing.controllers;
+
+import com.desk_sharing.entities.Desk;
+import com.desk_sharing.services.DeskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:3000")
+@RequestMapping("/desks")
+public class DeskController {
+
+    @Autowired
+    DeskService deskService;
+
+    @PostMapping
+    public ResponseEntity<Desk> createDesk(@RequestBody Desk desk) {
+        Desk savedDesk = deskService.saveDesk(desk);
+        return new ResponseEntity<>(savedDesk, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Desk>> getAllDesks() {
+        List<Desk> desks = deskService.getAllDesks();
+        return new ResponseEntity<>(desks, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Desk> getDeskById(@PathVariable("id") Long id) {
+        Optional<Desk> desk = deskService.getDeskById(id);
+        return desk.map(value -> new ResponseEntity<>(value, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Desk> updateDesk(@PathVariable("id") Long id, @RequestBody Desk desk) {
+        Desk updatedDesk = deskService.updateDesk(id, desk);
+        return new ResponseEntity<>(updatedDesk, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteDesk(@PathVariable("id") Long id) {
+        deskService.deleteDesk(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+}
