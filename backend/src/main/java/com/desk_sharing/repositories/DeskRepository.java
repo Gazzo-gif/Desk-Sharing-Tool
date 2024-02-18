7daa385346fdb2e2a86375049e56186b6f7a073a
Gazzo <gazzobahman@outlook.com>
2024-02-18 15:30:41 +0100

    desk by room id

--- backend/src/main/java/com/desk_sharing/repositories/DeskRepository.java
+++ backend/src/main/java/com/desk_sharing/repositories/DeskRepository.java
@@ -3,5 +3,8 @@ package com.desk_sharing.repositories;
 import com.desk_sharing.entities.Desk;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface DeskRepository extends JpaRepository<Desk, Long> {
+    List<Desk> findByRoomId(Long roomId);
 }
