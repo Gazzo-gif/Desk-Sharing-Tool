064f4d7b92cdd0006a0f9168f814df594f5515bd
iomullony <isabel@rugania.es>
2024-03-02 20:14:41 +0100

    localization

--- frontend/src/i18n.js
+++ frontend/src/i18n.js
@@ -1,6 +1,5 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-
 import enTranslation from './locales/en.json';
 import deTranslation from './locales/de.json';
 
@@ -11,7 +10,7 @@ i18n
       en: { translation: enTranslation },
       de: { translation: deTranslation }
     },
-    lng: 'en', // default language
+    lng: localStorage.getItem('language') || navigator.language.split('-')[0], // Use browser language if not set
     fallbackLng: 'en',
     interpolation: {
       escapeValue: false
