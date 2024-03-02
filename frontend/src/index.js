4694b0dddf45be2251af12c8188c0d36f9fd9dd0
iomullony <isabel@rugania.es>
2024-03-02 20:09:52 +0100

    localization

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import { I18nextProvider } from 'react-i18next'; // Import the I18nextProvider
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import i18n from './i18n'; // Import the i18n instance
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <I18nextProvider i18n={i18n}> {/* Wrap your App component with the I18nextProvider */}
+      <App />
+    </I18nextProvider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
