4d30d9d4b0b9a675b0f9eb9593cee11b2923277b
iomullony <isabel@rugania.es>
2024-03-04 17:05:06 +0000

    createRoot

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { I18nextProvider } from 'react-i18next'; // Import the I18nextProvider
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import i18n from './i18n'; // Import the i18n instance
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <I18nextProvider i18n={i18n}> {/* Wrap your App component with the I18nextProvider */}
