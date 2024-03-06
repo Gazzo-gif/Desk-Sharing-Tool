9de61f42f55997400ab1dc25a1e49393facc2aca
iomullony <isabel@rugania.es>
2024-03-06 23:20:57 +0100

    sidebar

--- frontend/src/Components/Home/SidebarComponent.jsx
+++ frontend/src/Components/Home/SidebarComponent.jsx
@@ -7,14 +7,12 @@ import { Sidebar, Menu, MenuItem, SubMenu } from "react-pro-sidebar";
 import { RiAdminFill } from "react-icons/ri";
 import { useNavigate } from "react-router-dom";
 import { useTranslation } from "react-i18next";
-import { FaLanguage } from "react-icons/fa";
 
 const SidebarComponent = () => {
-  const { t, i18n } = useTranslation();
+  const { t } = useTranslation();
   const [tab, setTab] = useState({ active: "calendar" });
   const [collapsed, setCollapsed] = useState(false);
   const navigate = useNavigate();
-  const isAdmin = true; // Set to true/false based on user's admin status
 
   const handleClick = (name) => {
     switch (name) {
@@ -34,13 +32,7 @@ const SidebarComponent = () => {
 
       case "admin":
         setTab({ active: "admin" });
-        navigate("/admin", { replace: true }); // Navigate to admin panel
-        break;
-
-      case "language":
-        const currentLanguage = i18n.language;
-        const newLanguage = currentLanguage === "en" ? "de" : "en";
-        i18n.changeLanguage(newLanguage);
+        navigate("/admin", { replace: true });
         break;
 
       default:
@@ -55,7 +47,12 @@ const SidebarComponent = () => {
         backgroundColor="#008444"
         width={collapsed ? "80px" : "200px"}
         style={{
-          height: "100%", // Ensure the sidebar takes up the full height
+          height: "100%",
+          [`&.active`]: {
+            backgroundColor: "#13395e",
+            color: "#b6c8d9",
+            overflow: "auto",
+          },
         }}
       >
         <Menu
@@ -72,7 +69,8 @@ const SidebarComponent = () => {
             active={tab?.active === "collapse" ? true : false}
             icon={<BsList />}
             onClick={() => handleClick("collapse")}
-          />
+          >
+          </MenuItem>
           <MenuItem
             active={tab?.active === "profile" ? true : false}
             icon={<CgProfile />}
@@ -80,15 +78,13 @@ const SidebarComponent = () => {
           >
             {t("profile")}
           </MenuItem>
-          {isAdmin && (
-            <MenuItem
-              active={tab?.active === "admin" ? true : false}
-              icon={<RiAdminFill />}
-              onClick={() => handleClick("admin")}
-            >
-              {t("admin")}
-            </MenuItem>
-          )}
+          <MenuItem
+            active={tab?.active === "admin" ? true : false}
+            icon={<RiAdminFill />}
+            onClick={() => handleClick("admin")}
+          >
+            {t("admin")}
+          </MenuItem>
           <MenuItem
             active={tab?.active === "calendar" ? true : false}
             icon={<IoCalendarNumberOutline />}
@@ -97,15 +93,9 @@ const SidebarComponent = () => {
             {t("calendar")}
           </MenuItem>
           <SubMenu icon={<CgDisplayFullwidth />} label={t("bookings")}>
-            <MenuItem>12/12/24</MenuItem>
-            <MenuItem>13/12/24</MenuItem>
+            <MenuItem> 12/12/24 </MenuItem>
+            <MenuItem> 13/12/24 </MenuItem>
           </SubMenu>
-          <MenuItem
-            icon={<FaLanguage />}
-            onClick={() => handleClick("language")}
-          >
-            {i18n.language === "en" ? "Deutsch" : "English"}
-          </MenuItem>
         </Menu>
       </Sidebar>
     </div>
