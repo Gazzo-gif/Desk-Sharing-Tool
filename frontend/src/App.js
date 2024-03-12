aa53d0d55a2d870097850667fc2de4b643e28182
burakalpyel <burakalpyel@gmail.com>
2024-03-12 09:45:11 +0100

    Profile Fixed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,6 @@ import Home from "./Components/Home/Home";
 import LoginPage from "./Components/LoginForm/LoginPage";
 import Floor from "./Components/Floors/Floor"; // Import the Floor component
 import Booking from "./Components/Home/Booking";
-import Profile from "./Components/Profile/Profile";
 import AdminPage from "./Components/AdminPanel/AdminPage"
 import {
   BrowserRouter as Router,
@@ -22,7 +21,6 @@ function App() {
           <Route path="/home" element={<Home />}></Route>
           <Route path="/floor" element={<Floor />}></Route> {/* Add the route for Floor */}
           <Route path="/desks" element={<Booking />}></Route>
-          <Route path="/profile" element={<Profile />}></Route>
           <Route path="/admin" element={<AdminPage />}></Route> {/* Add route for AdminPanel */}
 
           <Route path="*" element={<Navigate to="/home" />} />
