f0f4bc379948e2ef931e7fe7c06ef1cff5e7ab47
iomullony <isabel@rugania.es>
2024-03-13 22:54:46 +0100

    room and translation

--- frontend/src/Components/Floors/Floor.jsx
+++ frontend/src/Components/Floors/Floor.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import firstFloorImage from '../../images/firstfloor.png';
@@ -89,3 +90,98 @@ const Floor = () => {
 };
 
 export default Floor;
+=======
+import React, { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import firstFloorImage from '../../images/firstfloor.png';
+import secondFloorImage from '../../images/secondfloor.png'; 
+import './Floor.css'; 
+import { useNavigate } from "react-router-dom";
+import SidebarComponent from "../Home/SidebarComponent"
+import { useTranslation } from "react-i18next";
+
+const Floor = () => {
+  const { t } = useTranslation();
+  const navigate = useNavigate();
+  const [selectedRoom, setSelectedRoom] = useState(null);
+  const [currentFloor, setCurrentFloor] = useState('Ground');
+  const [rooms, setRooms] = useState([]);
+  const location = useLocation();
+  const { date } = location.state || {};
+  const formattedDate = date ? new Date(date).toLocaleDateString() : '';
+  const [filterType, setFilterType] = useState(null);
+
+  useEffect(() => {
+    // Fetch room data from the backend
+    fetch('/rooms/status')
+      .then(response => response.json())
+      .then(data => {
+        // Apply filter if selected
+        let filteredRooms = data.filter(room => room.floor === currentFloor);
+        if (filterType === 'Silence') {
+          filteredRooms = filteredRooms.filter(room => room.type === 'Silence');
+        } else if (filterType === 'Normal') {
+          filteredRooms = filteredRooms.filter(room => room.type === 'Normal');
+        }
+        setRooms(filteredRooms);
+      })
+      .catch(error => {
+        console.error('Error fetching room data:', error);
+      });
+  }, [currentFloor, filterType]);
+
+  const handleRoomClick = (roomId) => {
+    setSelectedRoom(roomId === selectedRoom ? null : roomId);
+    navigate("/desks", { state: { roomId, date } });
+  };
+
+  const toggleFloor = () => {
+    setCurrentFloor(currentFloor === 'Ground' ? 'First' : 'Ground');
+    setSelectedRoom(null); // Reset selected room when changing floors
+  };
+
+  function back() {
+    navigate(-1);
+  }
+
+  const floorImage = currentFloor === 'Ground' ? firstFloorImage : secondFloorImage;
+
+  return (
+    <div style={{ display: 'flex', width: '100vw', height: '100vh' }}>
+      <div className="sidebar">
+        <SidebarComponent />
+      </div>
+
+      <div className="floor-content">
+        <h1>{currentFloor === 'Ground' ? t("groundFloor") : t("firstFloor")}</h1>
+        {date && <p>{t("chosenDate")}: {formattedDate}</p>}
+        <div>
+          <select id="filterType" value={filterType} onChange={(e) => setFilterType(e.target.value)}>
+            <option value="">{t("all")}</option>
+            <option value="Normal">{t("normal")}</option>
+            <option value="Silence">{t("silence")}</option>
+          </select>
+        </div>
+        <button onClick={toggleFloor}>{t("switchFloor")}</button>
+        <div className="image-wrapper">
+          <img src={floorImage} alt={`${currentFloor === 'Ground' ? 'Ground' : 'First'} Floor`} />
+          {rooms.map((room) => (
+            <div
+              key={room.id}
+              className={`clickable-area ${selectedRoom === room.id ? 'selected' : ''}`}
+              style={{ top: `${room.x}px`, left: `${room.y}px` }}
+              onClick={() => handleRoomClick(room.id)}
+            ></div>
+          ))}
+        </div>
+        <div className="backButtonDiv">
+          <button className="backButton" onClick={back}>Back</button>
+        </div>
+      </div>
+
+    </div>
+  );
+};
+
+export default Floor;
+>>>>>>> 7e5996b (room and translation)
--- frontend/src/Components/Home/Booking.jsx
+++ frontend/src/Components/Home/Booking.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
 import React, { useState, useEffect } from "react";
 import { confirmAlert } from "react-confirm-alert";
@@ -274,3 +275,282 @@ const Booking = () => {
 };
 
 export default Booking;
+=======
+import React, { useState, useEffect } from "react";
+import { confirmAlert } from "react-confirm-alert";
+import "react-confirm-alert/src/react-confirm-alert.css";
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
+import "./HomeCalendar.scss";
+import "./Booking.css";
+import SidebarComponent from "./SidebarComponent"
+import { useTranslation } from "react-i18next";
+import { useNavigate, useLocation  } from 'react-router-dom';
+import { toast } from 'react-toastify';
+
+const Booking = () => {
+  const { t, i18n } = useTranslation();
+  const navigate = useNavigate();
+  const location = useLocation();
+  const localizer = momentLocalizer(moment);
+  const { roomId, date } = location.state;
+  const [desks, setDesks] = useState([]);
+  const [deskEvents, setDeskEvents] = useState([]);
+  const [events, setEvents] = useState([]);
+  const [event, setEvent] = useState({});
+  const [clickedDeskId, setClickedDeskId] = useState(null);
+
+  useEffect(() => {
+    const fetchDesks = async () => {
+      try {
+        const response = await fetch(`/desks/room/${roomId}`, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+
+        if (!response.ok) {
+          throw new Error("Error fetching desk data");
+        }
+
+        const data = await response.json();
+        setDesks(data);
+      } catch (error) {
+        console.error("Error fetching desk data:", error);
+      }
+    };
+
+    if (roomId) {
+      fetchDesks();
+    }
+  }, [roomId]);
+
+  useEffect(() => {
+    moment.locale(i18n.language);
+    setEvents([...events]);
+  }, [i18n.language]);
+
+  useEffect(() => {
+    const handleDeskClick = async (desk) => {
+      try {
+        const response = await fetch(
+          `/bookings/desk/${desk.id}`,
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+    
+        if (!response.ok) {
+          throw new Error("Error fetching desk booking data");
+        }
+    
+        const bookingData = await response.json();
+        console.log("Booking data for desk:", bookingData);
+    
+        // Parse the booking data and add events to tempArray
+        const bookingEvents = bookingData.map((booking) => ({
+          start: new Date(booking.day + 'T' + booking.begin),
+          end: new Date(booking.day + 'T' + booking.end),
+          title: booking.user.name,
+          id: 0,
+        }));
+        
+        setDeskEvents(bookingEvents);
+        setEvents(bookingEvents);
+        setClickedDeskId(desk.id);
+      } catch (error) {
+        console.error("Error fetching desk booking data:", error);
+      }
+    };
+
+    desks.forEach(desk => {
+      if (desk.id === clickedDeskId) {
+        handleDeskClick(desk);
+      }
+    });
+  }, [desks, clickedDeskId]);
+
+  const gg = (data) => {
+    const startTime = new Date(data.start);
+    const endTime = new Date(data.end);
+  
+    // Calculate the duration in milliseconds
+    const duration = endTime - startTime;
+  
+    // Check if the duration is within the allowed range
+    if (duration < 2 * 60 * 60 * 1000) {
+      alert("Minimum booking duration is 2 hours.");
+      return;
+    }
+  
+    if (duration > 9 * 60 * 60 * 1000) {
+      alert("Maximum booking duration is 9 hours.");
+      return;
+    }
+  
+    // Remove the existing event being created if any
+    const updatedEvents = events.filter(existingEvent => existingEvent.id !== event.id);
+  
+    // Check for overlapping events for the specific desk
+    const isOverlap = updatedEvents.some((existingEvent) =>
+      (existingEvent.start <= startTime && startTime < existingEvent.end) ||
+      (existingEvent.start < endTime && endTime <= existingEvent.end) ||
+      (startTime <= existingEvent.start && existingEvent.end <= endTime)
+    );
+  
+    if (isOverlap) {
+      alert("This slot overlaps with another booking for this desk. Please choose a different slot.");
+      return;
+    }
+  
+    const newEvent = {
+      start: data.start,
+      end: data.end,
+      id: 1,
+    };
+  
+    // Update events state with existing events and the new event
+    setEvents([...deskEvents, newEvent]);
+    setEvent(newEvent);
+  };
+
+  const booking = async () => {
+    if (!clickedDeskId || !event.start || !event.end) {
+      alert("Please choose a desk and time before booking.");
+      return;
+    }  
+
+    const userId = localStorage.getItem("userId");
+    const room_Id = roomId;
+    const deskId = clickedDeskId;
+    const day = moment(event.start).format("YYYY-MM-DD");
+    const start = moment(event.start).format("HH:mm:ss");
+    const ending = moment(event.end).format("HH:mm:ss");
+
+    confirmAlert({
+      title: "Book Desk " + clickedDeskId,
+      message: "For day " + day + "\nFrom " + start + " to " + ending,
+      buttons: [
+        {
+          label: "Yes",
+          onClick: async () => {
+            const bookingData = {
+              user_id: userId,
+              room_id: room_Id,
+              desk_id: deskId,
+              day: day,
+              begin: start,
+              end: ending
+            };
+
+            try {
+              const response = await fetch("/bookings", {
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/json"
+                },
+                body: JSON.stringify(bookingData)
+              })
+
+              if (!response.ok) {
+                throw new Error("Error fetching desk booking data");
+              }
+          
+              const data = await response.json();
+              console.log("Booking saved successfully:", data);
+              toast("Booking saved successfully");
+
+              const booking = {
+                id: data.id,
+                title: `Desk ${data.deskId}`,
+                start: new Date(`${data.day}T${data.begin}`),
+                end: new Date(`${data.day}T${data.end}`)
+              }
+
+              navigate("/home", { state: { booking }, replace: true });
+            } catch (error) {
+              console.error("Error saving booking:", error);
+            }
+          },
+        },
+        {
+          label: "No",
+        },
+      ],
+    });
+  };
+
+  function back() {
+    navigate(-1);
+  }
+
+  return (
+    <div className="desk-page">
+      <div>
+        <SidebarComponent />
+      </div>
+      <div>
+        <button onClick={back}>Back</button>
+      </div>
+      <div className="container">
+        <div className="choose-date">
+          <h1>{t("availableDesks")}</h1>
+        </div>
+
+        <div className="info-container">
+          <div>
+            {desks.map((desk, index) => (
+              <div className="desk-component" key={index}>
+                <div>{desk.id}.</div>
+                <div className={`desk-description ${desk.id === clickedDeskId ? 'clicked' : ''}`} 
+                  onClick={() => setClickedDeskId(desk.id)}
+                >
+                  <p className="item-name">{desk.equipment}</p>
+                  <p className="item-taken">Some free slots</p>
+                </div>
+              </div>
+            ))}
+          </div>
+          <div>
+            <div className="calendar-container">
+              <Calendar
+                localizer={localizer}
+                events={events}
+                startAccessor="start"
+                endAccessor="end"
+                views={['day', 'week']}
+                defaultView="day"
+                defaultDate={date}
+                onSelectSlot={(data) => {
+                  if (clickedDeskId !== null) {
+                    gg(data);
+                  }
+                }}
+                selectable={true}
+                min={new Date(0, 0, 0, 6, 0, 0)} // 6 am
+                max={new Date(0, 0, 0, 22, 0, 0)} // 10 pm
+                eventPropGetter={(event) => ({
+                  style: {
+                    backgroundColor: deskEvents.some((deskEvent) => deskEvent.id === event.id)
+                      ? "grey" // Color for events from deskEvents
+                      : "#008444", // Color for new events
+                  },
+                })}
+              />
+            </div>
+            <button className="submit-btn" onClick={() => booking()}>
+              {t("book")}
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Booking;
+>>>>>>> 7e5996b (room and translation)
