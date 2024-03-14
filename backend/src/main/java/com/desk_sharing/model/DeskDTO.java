86ba037ba1befddcad0140173fd4de1cf3b677dc
iomullony <isabel@rugania.es>
2024-03-14 14:03:10 +0100

    localization

--- backend/src/main/java/com/desk_sharing/model/DeskDTO.java
+++ backend/src/main/java/com/desk_sharing/model/DeskDTO.java
@@ -2,18 +2,30 @@ package com.desk_sharing.model;
 
 public class DeskDTO {
 
+	private Long deskId;
 	private Long roomId;
 	private String equipment;
+
+	public Long getDeskId() {
+		return deskId;
+	}
+
+	public void setDeskId(Long deskId) {
+		this.deskId = deskId;
+	}
 
 	public Long getRoomId() {
 		return roomId;
 	}
+
 	public void setRoomId(Long roomId) {
 		this.roomId = roomId;
 	}
+
 	public String getEquipment() {
 		return equipment;
 	}
+	
 	public void setEquipment(String equipment) {
 		this.equipment = equipment;
 	}
