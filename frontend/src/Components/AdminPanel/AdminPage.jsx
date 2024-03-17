a514437a212c144f3a8ba81c3f54542c3e31ee0f
George Nyagura <97040916+georgeNyagura@users.noreply.github.com>
2024-03-17 22:01:53 +0200

    Added desks to Admin panel

--- frontend/src/Components/AdminPanel/AdminPage.jsx
+++ frontend/src/Components/AdminPanel/AdminPage.jsx
@@ -1,13 +1,43 @@
-import React, { useState } from 'react';
-import SidebarComponent from '../Home/SidebarComponent';
+import React, { useState } from "react";
+import SidebarComponent from "../Home/SidebarComponent";
 import { FaAddressBook, FaPlusMinus } from "react-icons/fa6";
 import { FaBook } from "react-icons/fa";
-import './AdminPage.css'; // Import the CSS file for AdminPage
+import "./AdminPage.css"; // Import the CSS file for AdminPage
 import { MdDownload } from "react-icons/md";
+import { useEffect } from "react";
 
 const AdminPage = ({ collapsed, onCollapse }) => {
+  const roomId = localStorage.getItem("roomId");
   const [activeButton, setActiveButton] = useState(null);
+  const [desks, setDesks] = useState([]);
+  useEffect(() => {
+    const fetchDesks = async () => {
+      try {
+        const response = await fetch(
+          `http://188.34.162.76:8080/desks/room/${roomId}`,
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
 
+        if (!response.ok) {
+          throw new Error("Error fetching desk data");
+        }
+
+        const data = await response.json();
+        setDesks(data);
+      } catch (error) {
+        console.error("Error fetching desk data:", error);
+      }
+    };
+
+    if (roomId) {
+      fetchDesks();
+    }
+  }, [roomId]);
   const toggleButtons = (button) => {
     if (activeButton === button) {
       setActiveButton(null); // Toggle off if the same button is clicked
@@ -25,28 +55,40 @@ const AdminPage = ({ collapsed, onCollapse }) => {
           <div className="admin-controls-container">
             {/* User Management button and logo */}
             <div className="user-management-container">
-              <button className="user-management-button" onClick={() => toggleButtons('user-management')}>
+              <button
+                className="user-management-button"
+                onClick={() => toggleButtons("user-management")}
+              >
                 User Management
               </button>
               <FaAddressBook className="logo" />
             </div>
             {/* Edit Rooms button and logo */}
             <div className="edit-rooms-container">
-              <button className="edit-rooms-button" onClick={() => toggleButtons('edit-rooms')}>
+              <button
+                className="edit-rooms-button"
+                onClick={() => toggleButtons("edit-rooms")}
+              >
                 Edit Rooms
               </button>
               <FaPlusMinus className="logo" />
             </div>
             {/* Manage Bookings button and logo */}
             <div className="manage-bookings-container">
-              <button className="manage-bookings-button" onClick={() => toggleButtons('manage-bookings')}>
+              <button
+                className="manage-bookings-button"
+                onClick={() => toggleButtons("manage-bookings")}
+              >
                 Manage Bookings
               </button>
               <FaBook className="logo" />
             </div>
             {/* Download Statistics button and logo */}
             <div className="download-statistics-container">
-              <button className="download-statistics-button" onClick={() => toggleButtons('download-statistics')}>
+              <button
+                className="download-statistics-button"
+                onClick={() => toggleButtons("download-statistics")}
+              >
                 Download Statistics
               </button>
               <MdDownload className="logo" />
@@ -54,41 +96,91 @@ const AdminPage = ({ collapsed, onCollapse }) => {
           </div>
           {/* Wrapper for employee, workstation, and booking buttons */}
           <div className="button-wrapper">
-            <div className={`employee-button-wrapper ${activeButton === 'user-management' ? 'visible' : ''}`}>
-              <button className="employee-button" onClick={() => console.log("Add Employee clicked")}>
+            <div
+              className={`employee-button-wrapper ${
+                activeButton === "user-management" ? "visible" : ""
+              }`}
+            >
+              <button
+                className="employee-button"
+                onClick={() => console.log("Add Employee clicked")}
+              >
                 Add Employee
               </button>
-              <button className="employee-button" onClick={() => console.log("Delete Employee clicked")}>
+              <button
+                className="employee-button"
+                onClick={() => console.log("Delete Employee clicked")}
+              >
                 Delete Employee
               </button>
-              <button className="employee-button" onClick={() => console.log("Edit Employee clicked")}>
+              <button
+                className="employee-button"
+                onClick={() => console.log("Edit Employee clicked")}
+              >
                 Edit Employee
               </button>
             </div>
-            <div className={`workstation-button-wrapper ${activeButton === 'edit-rooms' ? 'visible' : ''}`}>
-              <button className="workstation-button" onClick={() => console.log("Add Workstation clicked")}>
+            <div
+              className={`workstation-button-wrapper ${
+                activeButton === "edit-rooms" ? "visible" : ""
+              }`}
+            >
+              <button
+                className="workstation-button"
+                onClick={() => console.log("Add Workstation clicked")}
+              >
                 Add Workstation
               </button>
-              <button className="workstation-button" onClick={() => console.log("Delete Workstation clicked")}>
+              <button
+                className="workstation-button"
+                onClick={() => console.log("Delete Workstation clicked")}
+              >
                 Delete Workstation
               </button>
-              <button className="workstation-button" onClick={() => console.log("Edit Workstation clicked")}>
+              <button
+                className="workstation-button"
+                onClick={() => console.log("Edit Workstation clicked")}
+              >
                 Edit Workstation
               </button>
             </div>
-            <div className={`booking-button-wrapper ${activeButton === 'manage-bookings' ? 'visible' : ''}`}>
-              <button className="booking-button" onClick={() => console.log("Delete Booking clicked")}>
+            <div
+              className={`booking-button-wrapper ${
+                activeButton === "manage-bookings" ? "visible" : ""
+              }`}
+            >
+              <button
+                className="booking-button"
+                onClick={() => console.log("Delete Booking clicked")}
+              >
                 Delete Booking
               </button>
-              <button className="booking-button" onClick={() => console.log("Edit Booking clicked")}>
+              <button
+                className="booking-button"
+                onClick={() => console.log("Edit Booking clicked")}
+              >
                 Edit Booking
               </button>
             </div>
           </div>
         </div>
+        <div className="bottom-container">
+          <div>jjjj</div>
+          <div className="desk-container">
+            {desks.map((desk, index) => (
+              <div className="desk-component" key={index}>
+                <div>{desk.id}.</div>
+                <div className="desk-description">
+                  <p className="item-name">{desk.equipment}</p>
+                  <p className="item-taken">Some free slots</p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
-}
+};
 
 export default AdminPage;
