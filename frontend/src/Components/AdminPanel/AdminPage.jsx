624b4a11781934de8957a3ce4acc457d110e26b5
George Nyagura <97040916+georgeNyagura@users.noreply.github.com>
2024-03-18 00:42:00 +0200

    Added Heat, Column, Usage Graphs

--- frontend/src/Components/AdminPanel/AdminPage.jsx
+++ frontend/src/Components/AdminPanel/AdminPage.jsx
@@ -5,10 +5,14 @@ import { FaBook } from "react-icons/fa";
 import "./AdminPage.css"; // Import the CSS file for AdminPage
 import { MdDownload } from "react-icons/md";
 import { useEffect } from "react";
+import { ColumnGraph } from "./ColumnGraph";
+import { HeatMap } from "./HeatMap";
+import { UsageGraph } from "./UsageGraph";
 
 const AdminPage = ({ collapsed, onCollapse }) => {
   const roomId = localStorage.getItem("roomId");
   const [activeButton, setActiveButton] = useState(null);
+  const [graph, setGraph] = useState("column-map");
   const [desks, setDesks] = useState([]);
   useEffect(() => {
     const fetchDesks = async () => {
@@ -50,122 +54,125 @@ const AdminPage = ({ collapsed, onCollapse }) => {
     <div className="adminhome-page">
       <SidebarComponent collapsed={collapsed} onCollapse={onCollapse} />
       <div className="adminpage-content">
-        <div className="admin-content">
-          <h1>Admin Page Content</h1>
-          <div className="admin-controls-container">
-            {/* User Management button and logo */}
-            <div className="user-management-container">
-              <button
-                className="user-management-button"
-                onClick={() => toggleButtons("user-management")}
-              >
-                User Management
-              </button>
-              <FaAddressBook className="logo" />
+        {true ? (
+          <></>
+        ) : (
+          <div className="admin-content">
+            <h1>Admin Page Content</h1>
+            <div className="admin-controls-container">
+              {/* User Management button and logo */}
+              <div className="user-management-container">
+                <button
+                  className="user-management-button"
+                  onClick={() => toggleButtons("user-management")}
+                >
+                  User Management
+                </button>
+                <FaAddressBook className="logo" />
+              </div>
+              {/* Edit Rooms button and logo */}
+              <div className="edit-rooms-container">
+                <button
+                  className="edit-rooms-button"
+                  onClick={() => toggleButtons("edit-rooms")}
+                >
+                  Edit Rooms
+                </button>
+                <FaPlusMinus className="logo" />
+              </div>
+              {/* Manage Bookings button and logo */}
+              <div className="manage-bookings-container">
+                <button
+                  className="manage-bookings-button"
+                  onClick={() => toggleButtons("manage-bookings")}
+                >
+                  Manage Bookings
+                </button>
+                <FaBook className="logo" />
+              </div>
+              {/* Download Statistics button and logo */}
+              <div className="download-statistics-container">
+                <button
+                  className="download-statistics-button"
+                  onClick={() => toggleButtons("download-statistics")}
+                >
+                  Download Statistics
+                </button>
+                <MdDownload className="logo" />
+              </div>
             </div>
-            {/* Edit Rooms button and logo */}
-            <div className="edit-rooms-container">
-              <button
-                className="edit-rooms-button"
-                onClick={() => toggleButtons("edit-rooms")}
+            {/* Wrapper for employee, workstation, and booking buttons */}
+            <div className="button-wrapper">
+              <div
+                className={`employee-button-wrapper ${
+                  activeButton === "user-management" ? "visible" : ""
+                }`}
               >
-                Edit Rooms
-              </button>
-              <FaPlusMinus className="logo" />
-            </div>
-            {/* Manage Bookings button and logo */}
-            <div className="manage-bookings-container">
-              <button
-                className="manage-bookings-button"
-                onClick={() => toggleButtons("manage-bookings")}
+                <button
+                  className="employee-button"
+                  onClick={() => console.log("Add Employee clicked")}
+                >
+                  Add Employee
+                </button>
+                <button
+                  className="employee-button"
+                  onClick={() => console.log("Delete Employee clicked")}
+                >
+                  Delete Employee
+                </button>
+                <button
+                  className="employee-button"
+                  onClick={() => console.log("Edit Employee clicked")}
+                >
+                  Edit Employee
+                </button>
+              </div>
+              <div
+                className={`workstation-button-wrapper ${
+                  activeButton === "edit-rooms" ? "visible" : ""
+                }`}
               >
-                Manage Bookings
-              </button>
-              <FaBook className="logo" />
-            </div>
-            {/* Download Statistics button and logo */}
-            <div className="download-statistics-container">
-              <button
-                className="download-statistics-button"
-                onClick={() => toggleButtons("download-statistics")}
+                <button
+                  className="workstation-button"
+                  onClick={() => console.log("Add Workstation clicked")}
+                >
+                  Add Workstation
+                </button>
+                <button
+                  className="workstation-button"
+                  onClick={() => console.log("Delete Workstation clicked")}
+                >
+                  Delete Workstation
+                </button>
+                <button
+                  className="workstation-button"
+                  onClick={() => console.log("Edit Workstation clicked")}
+                >
+                  Edit Workstation
+                </button>
+              </div>
+              <div
+                className={`booking-button-wrapper ${
+                  activeButton === "manage-bookings" ? "visible" : ""
+                }`}
               >
-                Download Statistics
-              </button>
-              <MdDownload className="logo" />
+                <button
+                  className="booking-button"
+                  onClick={() => console.log("Delete Booking clicked")}
+                >
+                  Delete Booking
+                </button>
+                <button
+                  className="booking-button"
+                  onClick={() => console.log("Edit Booking clicked")}
+                >
+                  Edit Booking
+                </button>
+              </div>
             </div>
           </div>
-          {/* Wrapper for employee, workstation, and booking buttons */}
-          <div className="button-wrapper">
-            <div
-              className={`employee-button-wrapper ${
-                activeButton === "user-management" ? "visible" : ""
-              }`}
-            >
-              <button
-                className="employee-button"
-                onClick={() => console.log("Add Employee clicked")}
-              >
-                Add Employee
-              </button>
-              <button
-                className="employee-button"
-                onClick={() => console.log("Delete Employee clicked")}
-              >
-                Delete Employee
-              </button>
-              <button
-                className="employee-button"
-                onClick={() => console.log("Edit Employee clicked")}
-              >
-                Edit Employee
-              </button>
-            </div>
-            <div
-              className={`workstation-button-wrapper ${
-                activeButton === "edit-rooms" ? "visible" : ""
-              }`}
-            >
-              <button
-                className="workstation-button"
-                onClick={() => console.log("Add Workstation clicked")}
-              >
-                Add Workstation
-              </button>
-              <button
-                className="workstation-button"
-                onClick={() => console.log("Delete Workstation clicked")}
-              >
-                Delete Workstation
-              </button>
-              <button
-                className="workstation-button"
-                onClick={() => console.log("Edit Workstation clicked")}
-              >
-                Edit Workstation
-              </button>
-            </div>
-            <div
-              className={`booking-button-wrapper ${
-                activeButton === "manage-bookings" ? "visible" : ""
-              }`}
-            >
-              <button
-                className="booking-button"
-                onClick={() => console.log("Delete Booking clicked")}
-              >
-                Delete Booking
-              </button>
-              <button
-                className="booking-button"
-                onClick={() => console.log("Edit Booking clicked")}
-              >
-                Edit Booking
-              </button>
-            </div>
-          </div>
-        </div>
+        )}
         <div className="bottom-container">
-          <div>jjjj</div>
           <div className="desk-container">
             {desks.map((desk, index) => (
               <div className="desk-component" key={index}>
@@ -176,6 +183,41 @@ const AdminPage = ({ collapsed, onCollapse }) => {
                 </div>
               </div>
             ))}
+          </div>
+          <div className="data-display">
+            <div className="maps">
+              <div onClick={() => setGraph("column-map")} className="map">
+                Column Map
+              </div>
+              <div onClick={() => setGraph("heat-map")} className="map">
+                Heat Map
+              </div>
+              <div onClick={() => setGraph("usage-map")} className="map">
+                Usage
+              </div>
+            </div>
+
+            {graph === "heat-map" ? (
+              <div className="heat">
+                <HeatMap />
+              </div>
+            ) : (
+              ""
+            )}
+            {graph === "column-map" ? (
+              <div className="column">
+                <ColumnGraph />
+              </div>
+            ) : (
+              ""
+            )}
+            {graph === "usage-map" ? (
+              <div className="heat">
+                <UsageGraph />
+              </div>
+            ) : (
+              ""
+            )}
           </div>
         </div>
       </div>
