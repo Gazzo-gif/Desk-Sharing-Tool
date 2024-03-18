89a10f7152ef8766094dbae0f4629f30844a82d8
iomullony <isabel@rugania.es>
2024-03-18 15:35:56 +0100

    merge gazzo

--- backend/src/main/java/com/desk_sharing/exception/GlobalExceptionHandling.java
+++ backend/src/main/java/com/desk_sharing/exception/GlobalExceptionHandling.java
@@ -1,15 +1,7 @@
 package com.desk_sharing.exception;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
-import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
