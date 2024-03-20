649ba22548e71341b72cedb2273f518173b579de
iomullony <isabel@rugania.es>
2024-03-20 04:28:43 +0100

    stats localization

--- frontend/src/Components/AdminPanel/AdminPage.jsx
+++ frontend/src/Components/AdminPanel/AdminPage.jsx
@@ -218,21 +218,21 @@ const AdminPage = () => {
               {deskList === "" ? (
                 <div className="desk-component">
                   <div className="desk-description-no-data">
-                    <p className="item-name">No items available</p>
+                    <p className="item-name">{t("deskNotFound")}</p>
                   </div>
                 </div>
               ) : (
                 deskList.map((desk, index) => (
                   <div
                     onClick={() => randomiseData()}
-                    // onClick={() => setGraph(`${tempActive}`)}
                     className="desk-component"
                     key={index}
                   >
                     <div>{desk.id}.</div>
                     <div className="desk-description">
-                      <p className="item-name">{desk.floor}</p>
-                      <p className="item-taken">Some free slots</p>
+                      <p className="item-name">
+                        {desk.floor === "Ground" ? t("groundFloor") : t("firstFloor")}</p>
+                      <p className="item-taken">{t("available")}</p>
                     </div>
                   </div>
                 ))
@@ -270,7 +270,7 @@ const AdminPage = () => {
                   onClick={() => setGraph("usage-map")}
                   className={graph === "usage-map" ? "usage-map" : "map"}
                 >
-                  Usage
+                  {t("usage")}
                 </div>
               </div>
               {deskList === "" ? (
